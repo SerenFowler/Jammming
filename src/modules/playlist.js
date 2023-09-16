6614356c9f234d379adf00be21ac2785725daaa8
Seren Fowler <serentfowler@protonmail.com>
2023-09-16 16:02:53 -0400

    created initial jsx for track and playlist

--- src/modules/playlist.js
+++ src/modules/playlist.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import Track from './track';
+
+export function Playlist({playlistInfo})
+{
+    return (
+        <div>
+            {playlistInfo.map((track) => <Track  trackInfo={track}/>)}
+        </div>
+    )
+}
--- src/modules/track.js
+++ src/modules/track.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+export function Track({trackInfo}) 
+{
+    return(
+        <div>
+            <h3>{trackInfo.title}</h3>
+            <p>{trackInfo.artist}</p>
+            <p>{trackInfo.genre}</p>
+        </div>
+    )
+}
