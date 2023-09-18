a30944cd24b7379675b713c12c1ce608383673c7
Seren Fowler <serentfowler@protonmail.com>
2023-09-18 14:33:00 -0400

    updated the searchbars placeholder to ask for artist and albums in addition to songs

--- src/modules/searchBar.js
+++ src/modules/searchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export function SearchBar() {
     return (
         <form>
-            <input type="text" placeholder="Search for a song."></input>
+            <input type="text" placeholder="Search for a song, artist, or album."></input>
             <input type="submit" value="Search"></input>
         </form>
     )
