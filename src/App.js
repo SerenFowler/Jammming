ed2fcdd78afe50c1a54da5c9683583a961526198
Seren Fowler <serentfowler@protonmail.com>
2023-09-18 15:21:04 -0400

    Actually hardcoded track array for testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 import { SearchBar } from './modules/searchBar';
 
+//Hardcodes for testing 
+let tracks = 
+[
+  {title: "I see Fire", artist: "Ed Sheeran", album: "The Hobbit Soundtrack", genre: "Folk"}, 
+  {title: "Fearless", artist: "Taylor Swift", album: "Fearless", genre: "Country"}, 
+  {title: "Fast Car", artist: "Tracy Chapman", album: "Tracy Chapman", genre: "Folk Rock"}, 
+  {title: "A case of you", artist: "Joni Mitchell", album: "Blue", genre: "Folk"}, 
+  {title: "Home", artist: "Gabriel Aplin", album: "Home", genre: "folk"}, 
+  {title: "", artist: "", album: "", genre: ""}, 
+  {title: "", artist: "", album: "", genre: ""}
+]
+
+//State Declarations
+
+//App
 function App() {
   return (
     <div className="App">
