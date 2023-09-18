a205ca192f0a62538d8a686cd3c4c8fabc80810d
Seren Fowler <serentfowler@protonmail.com>
2023-09-17 20:53:29 -0400

    adding search bar and updating App.js to render it

--- src/modules/searchBar.js
+++ src/modules/searchBar.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Search for a song."></input>
+            <input type="submit" value="Search"></input>
+        </form>
+    )
+}
