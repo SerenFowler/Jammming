c6cedfc904aed0c5006410de9d9aaebc4ecf0887
Seren Fowler <serentfowler@protonmail.com>
2023-09-18 15:40:03 -0400

    updated track.js to include keys

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { SearchBar } from './modules/searchBar';
 //Hardcodes for testing 
 let tracks = 
 [
-  {title: "I see Fire", artist: "Ed Sheeran", album: "The Hobbit Soundtrack", genre: "Folk"}, 
-  {title: "Fearless", artist: "Taylor Swift", album: "Fearless", genre: "Country"}, 
-  {title: "Fast Car", artist: "Tracy Chapman", album: "Tracy Chapman", genre: "Folk Rock"}, 
-  {title: "A case of you", artist: "Joni Mitchell", album: "Blue", genre: "Folk"}, 
-  {title: "Home", artist: "Gabriel Aplin", album: "Home", genre: "folk"}, 
+  {title: "I see Fire", artist: "Ed Sheeran", album: "The Hobbit Soundtrack", genre: "Folk", id:1}, 
+  {title: "Fearless", artist: "Taylor Swift", album: "Fearless", genre: "Country", id:2}, 
+  {title: "Fast Car", artist: "Tracy Chapman", album: "Tracy Chapman", genre: "Folk Rock", id:3}, 
+  {title: "A case of you", artist: "Joni Mitchell", album: "Blue", genre: "Folk", id:4}, 
+  {title: "Home", artist: "Gabriel Aplin", album: "Home", genre: "folk", id:5}, 
   {title: "", artist: "", album: "", genre: ""}, 
   {title: "", artist: "", album: "", genre: ""}
 ]
