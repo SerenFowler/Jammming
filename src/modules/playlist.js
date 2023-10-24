0cbab61cb3941913cc1950222842e2f09f14b004
Seren Fowler <serentfowler@protonmail.com>
2023-10-24 19:37:05 -0400

    made it sy typing in the text field updates the name of the playlist.

--- src/modules/playlist.js
+++ src/modules/playlist.js
@@ -3,9 +3,18 @@ import { Track } from './track';
 
 export function Playlist(props)
 {
+    const handleChange = (e) => {
+        //e.preventDefault();  //found to be not inherently nessicary
+        let clonePlaylist = structuredClone(props.playlists[props.selectedPlaylist]);
+        clonePlaylist.playlistName = e.target.value;
+        let clonePlaylistShell = [...props.playlists];
+        clonePlaylistShell.splice(props.selectedPlaylist, 1, clonePlaylist);
+        props.setPlaylists(clonePlaylistShell);
+    }
+
     return (
         <div  className='playlist' key={props.playlistInfo.id}>
-            <input value={props.playlistInfo.playlistName} type="text"></input>
+            <input value={props.playlistInfo.playlistName} type="text" onChange={handleChange}></input>
             {props.playlistInfo.tracks.map((track) => <Track trackInfo={track} isPlaylist={true} setPlaylists={props.setPlaylists} playlists={props.playlists} selectedPlaylist={props.selectedPlaylist} />)}
         </div>
     )
