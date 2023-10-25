f9cd19b2bb5873b8a4e50bb66691232370f4a57a
Seren Fowler <serentfowler@protonmail.com>
2023-10-25 17:52:56 -0400

    prevented default when submitting searchBar.js, therefore preventing the page from reloading.

--- src/modules/searchBar.js
+++ src/modules/searchBar.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 export function SearchBar() 
 {
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input className='searchBar' type="text" placeholder="Search for a song, artist, or album."></input>
             <input type="submit" value="Search"></input>
         </form>
