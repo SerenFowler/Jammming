e39b8720ba1d101c8e811655518ecf3871017c6a
Seren Fowler <serentfowler@protonmail.com>
2024-02-03 15:15:38 -0500

    Added function body ro generateRandomString and sha256.

--- src/util/Authorize.js
+++ src/util/Authorize.js
@@ -1,3 +1,5 @@
+import { isLabelWithInternallyDisabledControl } from "@testing-library/user-event/dist/utils";
+
 const clientId = '66d9d5b9c29b4215bfd7adc0f269236c';
 const clientSecret = 'f0ac6e1b071d4ac286745020e2d8b4b4';
 
@@ -34,9 +36,15 @@ const generateRandomString = (length) =>
 {
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc+possible[x % possible.length], "");
 }
 
-const sha256 = async (plain) => {}
+const sha256 = async (plain) => 
+{
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest('SHA-256', data);
+}
 
 const base64encode = (input) => {}
 
